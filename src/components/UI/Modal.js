48b310293ae07897a1f295aeedb8a23c7879e8cf
shinyKavery <kaverypandanda@instancelatam.com>
2021-12-07 14:39:06 -0300

    feat(Modal): created a modal component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.close} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlay");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      <Backdrop close={props.onhideModal} />,
+      <ModalOverlay>{props.children}</ModalOverlay>,
+    </Fragment>
+  );
+};
+
+export default Modal;
