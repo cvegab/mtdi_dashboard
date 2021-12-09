9a8ba26c6d0b4d7b7edc506937ffc3da545292cb
shinyKavery <kaverypandanda@instancelatam.com>
2021-12-09 13:40:07 -0300

    feat(modal): added a modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,15 +2,18 @@ import { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import classes from "./Modal.module.css";
+import Card from './Card';
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.close} />;
 };
 
 const ModalOverlay = (props) => {
+  console.log(props.children);
   return (
     <div className={classes.modal}>
-      <div className={classes.content}>{props.children}</div>
+      <h1>My modal</h1>
+      <div className={classes.hea}><h2>{props.children}</h2></div>
     </div>
   );
 };
@@ -23,5 +26,24 @@ const Modal = (props) => {
     </Fragment>
   );
 };
+
+// const Modal = (props) => {
+//   return (
+//     <div>
+//       <div className={classes.backdrop} />
+//       <Card className={classes.modal}>
+//         <header className={classes.header}>
+//           <h2>hello</h2>
+//         </header>
+//         <div className={classes.hea}>
+//           <p><h2>hello shiny</h2></p>
+//         </div>
+//         <footer className={classes.actions}>
+//          <button>okay</button>
+//         </footer>
+//       </Card>
+//     </div>
+//   );
+// };
 
 export default Modal;
