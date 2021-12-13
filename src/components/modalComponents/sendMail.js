efd4afd14bf82b9a090544e4f48c8ff52a1bc055
shinyKavery <kaverypandanda@instancelatam.com>
2021-12-13 11:47:44 -0300

    added form

--- src/components/modalComponents/sendMail.js
+++ src/components/modalComponents/sendMail.js
@@ -1,9 +1,30 @@
 import Modal from "../UI/Modal";
+import { Button,Form } from "react-bootstrap";
 const SendMail = (props) => {
     console.log('i am from sendmail amd i was called');
   return (
     <Modal onhideModal={props.onhideModal}>
    <h3>Enviar documento tributario</h3>
+   <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
     </Modal>
   );
 };
