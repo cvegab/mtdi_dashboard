30804a8f1af4aa0f9b07cc4103472e1559626a15
cvegab <84888602+cvegab@users.noreply.github.com>
2022-01-13 18:10:21 -0300

    Revert "Update OrderMobileCard.js"

--- src/components/OrderMobileCard/OrderMobileCard.js
+++ src/components/OrderMobileCard/OrderMobileCard.js
@@ -235,7 +235,8 @@ function OrderMobileCard(props) {
                       <Accordion.Item eventKey="0">
                         <Accordion.Header>
                           <strong>Ver más</strong>
-                                                                       
+                                                   
+                         
                         </Accordion.Header>
                         <Accordion.Body>
 
@@ -379,6 +380,7 @@ function OrderMobileCard(props) {
                        
                       </tr> */}
 
+
 {/* HUB DE PAGO */}
                           
                       {/* <tr>
@@ -395,7 +397,8 @@ function OrderMobileCard(props) {
                         <td className="td-actions text-right">
                           <br/>
                           <br/>
-                        </td>                    
+                        </td>
+                       
                       </tr> */}
 
 {/* TOTAL */}
@@ -411,9 +414,9 @@ function OrderMobileCard(props) {
                         <td className="td-actions text-right">
                             <br/>
                             <br/>
-                        </td>             
+                        </td>
+                       
                       </tr>
-
 {/* SHIPPING */}
                       {/* <tr>
                         <td>
@@ -467,8 +470,8 @@ function OrderMobileCard(props) {
                           <br/>
                         </td>                  
                       </tr> */}
-
-{/* JEFE OPS */}
+{/* 
+JEFE OPS */}
                       {/* <tr>
                         <td>                   
                         </td>
