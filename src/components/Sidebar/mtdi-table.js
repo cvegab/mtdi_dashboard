2043224e739e6cd133d4931882e37d924ae10d48
shinyKavery <kaverypandanda@instancelatam.com>
2022-01-14 17:09:37 -0300

    added logic

--- src/components/Sidebar/mtdi-table.js
+++ src/components/Sidebar/mtdi-table.js
@@ -597,11 +597,39 @@ const MtdiTable = (props) => {
       console.log(error);
     }
   };
-  const incrementPageHandler = ()=>{
-let pageCount = 1;
-let incrementedPage;
-incrementedPage = pageCount++;
-console.log(incrementedPage);
+  const incrementPageHandler = async()=>{
+setpageCount(pageCount+1);
+let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=10&user=admin&channel=${channelId}&store=${tiendaId}&page=1&country=${paisId}&dateFrom=${selectedDate}&dateTo=${new Date()
+      .toISOString()
+      .slice(0, 10)}`;
+    console.log(url);
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
+    myHeaders.append(
+      "Authorization",
+      "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
+    );
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
+    try {
+      const response = await fetch(url, requestOptions);
+      if (!response.ok) {
+        throw new Error();
+      }
+      const data = await response.json();
+      // console.log(data);
+
+      setData(data.message);
+
+      setisLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   }
   const columns = [
     {
