045957a7e2c7b9ea9a5433b9d3e87b9fde987d64
valvidela <valeriavidela@instancelatam.com>
2022-01-14 22:07:33 +0100

    Test to pagination
    add new function to incrementPageHandler but have some errors

--- src/components/Sidebar/mtdi-table.js
+++ src/components/Sidebar/mtdi-table.js
@@ -47,9 +47,11 @@ const MtdiTable = (props) => {
   const [pageCount, setpageCount] = useState(1);
   const [paisId, setpaisId] = useState(1);
   const [tiendaId, settiendaId] = useState(0);
-  const [selectedDate, setselectedDate] = useState("");
+  const d = new Date();
+  d.setMonth(d.getMonth() - 6);
+  const [selectedDate, setselectedDate] = useState(d.toISOString().slice(0, 10));
   const [country, setcountry] = useState("");
-  const [channelId, setchannelId] = useState();
+  const [channelId, setchannelId] = useState(0);
   const [buyer, setbuyer] = useState("");
   const [salesChannel, setsalesChannel] = useState("");
   const [store, setstore] = useState("");
@@ -529,8 +531,9 @@ const MtdiTable = (props) => {
     };
     try {
       const response = await fetch(
-        `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=20&user=admin&store=0&page=${countPage}country=${paisId}&dateFrom=2021-12-01&dateTo=2021-12-03`,
-
+        `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=100&user=admin&channel=${channelId}&store=${tiendaId}&0&page=${pageCount}&country=${paisId}&dateFrom=${selectedDate}&dateTo=${new Date()
+        .toISOString()
+        .slice(0, 10)}`,
         requestOptions
       );
       if (!response.ok) {
@@ -565,7 +568,7 @@ const MtdiTable = (props) => {
   const applyFiltersButtonhandler = async () => {
     console.log("hi i was clicked");
     console.log(urlState);
-    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=10&user=admin&channel=${channelId}&store=${tiendaId}&page=1&country=${paisId}&dateFrom=${selectedDate}&dateTo=${new Date()
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=100&user=admin&channel=${channelId}&store=${tiendaId}&page=1&country=${paisId}&dateFrom=${selectedDate}&dateTo=${new Date()
       .toISOString()
       .slice(0, 10)}`;
     console.log(url);
@@ -597,12 +600,42 @@ const MtdiTable = (props) => {
       console.log(error);
     }
   };
-  const incrementPageHandler = ()=>{
-let pageCount = 1;
-let incrementedPage;
-incrementedPage = pageCount++;
-console.log(incrementedPage);
-  }
+
+  const incrementPageHandler = async()=>{
+    setpageCount(pageCount+1);
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/orders?qty=100&user=admin&channel=${channelId}&store=${tiendaId}&page=${pageCount}&country=${paisId}&dateFrom=${selectedDate}&dateTo=${new Date()
+          .toISOString()
+          .slice(0, 10)}`;
+        console.log(url);
+        var myHeaders = new Headers();
+        myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
+        myHeaders.append(
+          "Authorization",
+          "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
+        );
+        myHeaders.append("Content-Type", "application/json");
+        var requestOptions = {
+          method: "GET",
+          redirect: "follow",
+          headers: myHeaders,
+        };
+        try {
+          const response = await fetch(url, requestOptions);
+          if (!response.ok) {
+            throw new Error();
+          }
+          const data = await response.json();
+          // console.log(data);
+          setData(...data, data.message);
+          setisLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+
+  
+
   const columns = [
     {
       title: "OpsId",
