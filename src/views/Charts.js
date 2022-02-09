4f25a6828b8ddc84d8d21c05653077cc875614a9
shinyKavery <kaverypandanda@instancelatam.com>
2022-02-09 10:58:07 -0300

    added code refactor

--- src/views/Charts.js
+++ src/views/Charts.js
@@ -216,54 +216,7 @@ function Charts() {
       })
       .catch((error) => console.log("error", error));
   };
-  // useEffect(() => {
-  //   if (selectedDateFrom) {
-  //     console.log(selectedDateFrom);
-  //   }
-  //   setisLoading(true);
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
-  //   myHeaders.append(
-  //     "Authorization",
-  //     "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
-  //   );
-
-  //   var requestOptions = {
-  //     method: "GET",
-  //     headers: myHeaders,
-  //     redirect: "follow",
-  //   };
-
-  //   fetch(
-  //     "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/store/resume?channels=2,7&store=3&dateFrom=2021-12-01&dateTo=2022-01-13&country=1",
-  //     requestOptions
-  //   )
-  //     .then((response) => response.text())
-  //     .then((result) => {
-  //       var obj = JSON.parse(result);
-  //       console.log(obj);
-
-  //       var z = obj[0].filter((item) => {
-  //         return item.date_created === selectedDateFrom;
-  //       });
-  //       console.log(z);
-
-  //       if (z === undefined) {
-  //         settotalIncome(0);
-  //         setdispatchCost(0);
-  //       } else {
-  //         settotalIncome(z[0].total);
-  //         setdispatchCost(z[0].shipping_total);
-  //       }
-  //       setisLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error", error);
-  //       settotalIncome(0);
-  //       setdispatchCost(0);
-  //       setisLoading(false);
-  //     });
-  // }, [selectedDateFrom]);
+  
   const changeDateHandler = (event) => {
     console.log("hi");
     console.log(event);
