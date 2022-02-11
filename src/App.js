fe34accdc0edbcffb1e3dd44be618b497fd82195
camilovegabarrios <cv@chambas.cl>
2022-02-11 11:03:10 -0300

    first
    change logysto view

--- src/App.js
+++ src/App.js
@@ -32,73 +32,73 @@ const App = () => {
        
   //   }, [])
  console.log(fn);
-  useEffect(() => {
-    var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
-    myHeaders.append(
-      "Authorization",
-      "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
-    );
-    myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify({
-      email: userName,
-      // password: "SXB8TbidQGv4Z/CuvvLWhbfFQxiHVQcb0BEZ7NTEhuQ=",
-      password: password,
-    });
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-    fetch(
-      "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/login",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => {
-        const obj = JSON.parse(result);
+  // useEffect(() => {
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
+  //   myHeaders.append(
+  //     "Authorization",
+  //     "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
+  //   );
+  //   myHeaders.append("Content-Type", "application/json");
+  //   var raw = JSON.stringify({
+  //     email: userName,
+  //     // password: "SXB8TbidQGv4Z/CuvvLWhbfFQxiHVQcb0BEZ7NTEhuQ=",
+  //     password: password,
+  //   });
+  //   var requestOptions = {
+  //     method: "POST",
+  //     headers: myHeaders,
+  //     body: raw,
+  //     redirect: "follow",
+  //   };
+  //   fetch(
+  //     "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/login",
+  //     requestOptions
+  //   )
+  //     .then((response) => response.text())
+  //     .then((result) => {
+  //       const obj = JSON.parse(result);
 
-        if (obj.first_name !== undefined) {
-          localStorage.setItem("first", obj.first_name);
-         setfn(localStorage.setItem("first", obj.first_name));
-        }
-        if (obj.last_name !== undefined) {
-          localStorage.setItem("last", obj.last_name);
-          setln(localStorage.setItem("last", obj.last_name));
+  //       if (obj.first_name !== undefined) {
+  //         localStorage.setItem("first", obj.first_name);
+  //        setfn(localStorage.setItem("first", obj.first_name));
+  //       }
+  //       if (obj.last_name !== undefined) {
+  //         localStorage.setItem("last", obj.last_name);
+  //         setln(localStorage.setItem("last", obj.last_name));
          
-        }
-        if (
-          obj.message === "Autorizado" ||
-          localStorage.getItem("name") !== null
-        ) {
-          setisAuthenticated(true);
-          localStorage.setItem("name", userName);
-          localStorage.setItem(
-            "password",
-            "SXB8TbidQGv4Z/CuvvLWhbfFQxiHVQcb0BEZ7NTEhuQ="
-          );
-        } else {
-          setisAuthenticated(false);
-        }
-      })
-      .catch((error) => console.log("error", error));
-  }, []);
-  if (userName === undefined && localStorage.getItem("name") === null) {
-    return (window.location.href = "https://dev.instancelatam.com/login");
-  }
+  //       }
+  //       if (
+  //         obj.message === "Autorizado" ||
+  //         localStorage.getItem("name") !== null
+  //       ) {
+  //         setisAuthenticated(true);
+  //         localStorage.setItem("name", userName);
+  //         localStorage.setItem(
+  //           "password",
+  //           "SXB8TbidQGv4Z/CuvvLWhbfFQxiHVQcb0BEZ7NTEhuQ="
+  //         );
+  //       } else {
+  //         setisAuthenticated(false);
+  //       }
+  //     })
+  //     .catch((error) => console.log("error", error));
+  // }, []);
+  // if (userName === undefined && localStorage.getItem("name") === null) {
+  //   return (window.location.href = "https://dev.instancelatam.com/login");
+  // }
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-        {!isAuthenticated && localStorage.getItem("name") === null && (
+        {/* {!isAuthenticated && localStorage.getItem("name") === null && (
           <Route
             component={() => {
               window.location.href = "https://dev.instancelatam.com/login";
               return null;
             }}
           />
-        )}
+        )} */}
         {!isAuthenticated && localStorage.getItem("name") !== null && (
           <Route path="/admin" render={(props) => <AdminLayout {...props} name={fn} lastName={ln}/>} />
         )}
