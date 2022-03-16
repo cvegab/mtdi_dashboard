7f56a99ea36aad806272c5e2eef4ce4d35b0a246
shinyKavery <kaverypandanda@instancelatam.com>
2022-03-16 15:38:04 -0300

    changed the api to production

--- src/views/mtdi_reports.js
+++ src/views/mtdi_reports.js
@@ -597,7 +597,7 @@ function MtdiReports() {
         },
         {
           label: "Mercadolibre",
-          backgroundColor: "yellow",
+          backgroundColor: "#FFDC00",
           borderRadius: "20px",
           stack: "2",
           borderRadius: 6,
@@ -710,7 +710,7 @@ function MtdiReports() {
         },
         {
           label: "Mercadolibre",
-          backgroundColor: "yellow",
+          backgroundColor: "#FFDC00",
           borderRadius: "20px",
           stack: "2",
           borderRadius: 6,
@@ -827,7 +827,7 @@ function MtdiReports() {
     })
     console.log(newChannelList);
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "2Rr4OFKHVL98TtlOCUnuNaA2v5w01Z11aI9vdQYJ");
+    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
     myHeaders.append(
       "Authorization",
       "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
@@ -839,7 +839,7 @@ function MtdiReports() {
       redirect: "follow",
     };
 
-    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/preproduction/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
     console.log(url);
 
     fetch(url, requestOptions)
@@ -985,7 +985,7 @@ function MtdiReports() {
     })
     console.log(newChannelList);
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "2Rr4OFKHVL98TtlOCUnuNaA2v5w01Z11aI9vdQYJ");
+    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
     myHeaders.append(
       "Authorization",
       "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
@@ -997,7 +997,7 @@ function MtdiReports() {
       redirect: "follow",
     };
 
-    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/preproduction/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
     console.log(url);
 
   fetch(url, requestOptions)
@@ -1670,7 +1670,7 @@ setlistaPie(listaSalesValue[0]);
   };
   const fetchResumenGraphDetails = () => {
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "2Rr4OFKHVL98TtlOCUnuNaA2v5w01Z11aI9vdQYJ");
+    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
     myHeaders.append(
       "Authorization",
       "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
@@ -1682,7 +1682,7 @@ setlistaPie(listaSalesValue[0]);
       redirect: "follow",
     };
 
-    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/preproduction/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
 
     fetch(url, requestOptions)
       .then((response) => response.text())
@@ -1812,7 +1812,7 @@ setlistaPie(listaSalesValue[0]);
     console.log("hi i am fetching");
     setisLoading(true);
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "2Rr4OFKHVL98TtlOCUnuNaA2v5w01Z11aI9vdQYJ");
+    myHeaders.append("x-api-key", "mbHqRHonVS4HrcTZPIjhd5tHYkgzgpm38pH8gPpj");
     myHeaders.append(
       "Authorization",
       "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
@@ -1824,7 +1824,7 @@ setlistaPie(listaSalesValue[0]);
       redirect: "follow",
     };
     //2021-12-01
-    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/preproduction/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/prod/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
     console.log(url);
     fetch(url, requestOptions)
       .then((response) => response.text())
@@ -4291,8 +4291,8 @@ console.log(selectedChannelsArray);
                             <i
                               className="fa fa-circle"
                               style={{
-                                color: "yellow",
-                                backgroundColor: "yellow",
+                                color: "#FFDC00",
+                                backgroundColor: "#FFDC00",
                                 borderRadius: "3px",
                               }}
                             />
@@ -4882,8 +4882,8 @@ console.log(selectedChannelsArray);
                                 <i
                                   className="fa fa-circle"
                                   style={{
-                                    color: "yellow",
-                                    backgroundColor: "yellow",
+                                    color: "#FFDC00",
+                                    backgroundColor: "#FFDC00",
                                     borderRadius: "3px",
                                   }}
                                 />
