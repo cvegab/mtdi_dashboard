3b1da11f3002ab88714f8fae4c476428cfac0840
shinyKavery <kaverypandanda@instancelatam.com>
2022-03-17 15:28:19 -0300

    added a  new componnet for stacked sales graph

---
+++ src/components/GraphComponent/stacked-sales-graph.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { Line, Bar, Pie, Chart } from "react-chartjs-2";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    CardTitle,
+    Row,
+    Col,
+    Button,
+    Input,
+    Spinner,
+    Badge,
+  } from "reactstrap";
+const StackedSalesGraph = (props)=>{
+    return(
+
+    )
+}
+export default StackedSalesGraph;
