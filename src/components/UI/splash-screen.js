aa16e9ba1d6af4416ea3272734aeaf1d1c2ab81a
shinyKavery <kaverypandanda@instancelatam.com>
2022-03-18 10:38:06 -0300

    code refactor

--- src/components/UI/splash-screen.js
+++ src/components/UI/splash-screen.js
@@ -6,15 +6,6 @@ const SplashScreen = (props) => {
     <React.Fragment>
         <div className={classes.backdrop}>
       <div className={classes.centered}>
-        {/* &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; */}
-        {/* <div id='spinner'>
-          <img
-            src={spinnerGif}
-            style={{ marginTop: "2em" }}
-            width="200"
-            alt="Cargando..."
-          /> */}
-       
           <img
             src={spinnerGif}
             style={{ marginTop: "2em" }}
@@ -22,10 +13,6 @@ const SplashScreen = (props) => {
             alt="Cargando..."
           />
           <p className={classes.message}>&nbsp;&nbsp;&nbsp;Cargando {props.message}</p>
-     
-          {/* <p style={{fontWeight: "bold", color: "#1D308E"}}>Cargando...</p>                   */}
-         
-     
       </div>
       </div>
     </React.Fragment>
