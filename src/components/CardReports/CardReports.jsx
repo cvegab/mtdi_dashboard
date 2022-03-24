8df6b47a9a186c8dd9409db9bee89c5322defb28
valvidela <valeriavidela@instancelatam.com>
2022-03-24 20:17:46 -0300

    Update CardReports.jsx
    add new rows and separate cards

--- src/components/CardReports/CardReports.jsx
+++ src/components/CardReports/CardReports.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CardReports.css';
 import { Col, Row } from 'reactstrap';
+import { DriveEta } from '@material-ui/icons';
 
 
 const CardReports = (props) => {
@@ -8,18 +9,24 @@ const CardReports = (props) => {
     <div className="card-report">
      
       <h1 className="title-cardReport"> {props.title} </h1>
-  
+        <div className="row">
+        <div className="col-6">
           <label className="subtitle-cardReport">{props.subtitle1}</label>
           <label className="value-cardReport">{props.value1}</label>
-   
+        </div>
+        <div className="col-6">
           <label className="subtitle-cardReport">{props.subtitle2}</label>
           <label className="value-cardReport">{props.value2}</label>    
+        </div>
+        <div className="col-6">
           <label className="subtitle-cardReport">{props.subtitle3}</label>
           <label className="value-cardReport">{props.value3}</label>
-  
+        </div>
+        <div className="col-6">
           <label className="subtitle-cardReport">{props.subtitle4}</label>
           <label className="value-cardReport">{props.value4}</label>
-        
+        </div>
+        </div>
       
     </div>
   )
