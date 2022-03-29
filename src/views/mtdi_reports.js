1540ab055765fd72d56e12fb84332c2c6910ea4d
shinyKavery <kaverypandanda@instancelatam.com>
2022-03-29 16:31:31 -0300

    added the differnt loaders for every graph

--- src/views/mtdi_reports.js
+++ src/views/mtdi_reports.js
@@ -178,6 +178,8 @@ function MtdiReports() {
   const [newListaSales, setnewListaSales] = useState([]);
   const [stackedSalesGraph, setstackedSalesGraph] = useState({});
   const [isStackedGraphLoading, setisStackedGraphLoading] = useState(false);
+  const [isStackedOrdersGraph, setisStackedOrdersGraph] = useState(false);
+  const [isPieChartLoading, setisPieChartLoading] = useState(false);
   useEffect(() => {
     // set initial value
     const mediaWatcher = window.matchMedia("(max-width: 767px)");
@@ -201,10 +203,12 @@ function MtdiReports() {
     getDateLabels();
     fetchResumenGraphDetails();
     fetchStackedGraphForSales();
+    fetchStackedGraphForOrders();
   }, []);
   useEffect(() => {
     fetchResumenGraphDetails();
     fetchStackedGraphForSales();
+    fetchStackedGraphForOrders();
   }, [channelId]);
 
   useEffect(() => {
@@ -223,6 +227,159 @@ function MtdiReports() {
       setcR(null);
     };
   }, []);
+  //FETCH STACKED GRAPH FOR ORDERS GRAPH
+  const fetchStackedGraphForOrders = () => { 
+    setisStackedOrdersGraph(true);
+    let newChannelList = cR.map(item=>{
+      return item.channel;
+    })
+    setisLoading(true);
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
+    myHeaders.append(
+      "Authorization",
+      "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
+    );
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    let url = `https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/store/resume?channels=${channelId}&store=${storeId}&dateFrom=${selectedDateFrom}&dateTo=${selectedDateTo}&country=${countryId}`;
+    fetch(url, requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        var obj = JSON.parse(result);
+        if(newChannelList.length===0){
+          newChannelList = ['Vtex', 'Linio', 'MercadoLibre', 'Exito', 'Ripley', 'Shopify', 'Paris', 'Magento', 'Woocommerce', 'Chambas', 'ListaTienda'];
+        }
+        let linio = [];
+       if(newChannelList.includes('Linio')){
+        linio = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Linio;
+            });
+             console.log(linio[0].Linio);
+            setnewlinioMonthly(linio[0].Linio);
+       }
+       if(!newChannelList.includes('Linio')){
+        setnewlinioMonthly(0);
+       }
+       let Vtex = [];
+       if(newChannelList.includes('Vtex')){
+        Vtex = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Vtex;
+            });
+             console.log(Vtex[0].Vtex);
+            setnewVtexMonthly(Vtex[0].Vtex);
+       }
+       if(!newChannelList.includes('Vtex')){
+        setnewVtexMonthly(0);
+       }
+       let ripley = [];
+       if(newChannelList.includes('Ripley')){
+        ripley = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Ripley;
+            });
+             console.log(ripley[0].Ripley);
+            setnewRipleyMonthly(ripley[0].Ripley);
+       }
+       if(!newChannelList.includes('Ripley')){
+       setnewRipleyMonthly(0);
+       }
+      let chambas = [];
+      if(newChannelList.includes('Chambas')){
+        chambas = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Chambas;
+            });
+             console.log(chambas[0].Chambas);
+            setnewChambasMonthly(chambas[0].Chambas);
+       }
+       if(!newChannelList.includes('Chambas')){
+        setnewChambasMonthly(0);
+       }
+       let magento = [];
+       if(newChannelList.includes('Magento')){
+        magento = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Magento;
+            });
+             console.log(magento[0].Magento);
+           setnewMagentoMonthly(magento[0].Magento);
+       }
+       if(!newChannelList.includes('Magento')){
+       setnewMagentoMonthly(0);
+       }
+       let woo = [];
+       if(newChannelList.includes('Woocommerce')){
+        woo = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Woocommerce;
+            });
+             console.log(woo[0].Woocommerce);
+           setnewWooCommerceMonthly(woo[0].Woocommerce);
+       }
+       if(!newChannelList.includes('Woocommerce')){
+        setnewWooCommerceMonthly(0);
+       }
+       let shopify = [];
+       if(newChannelList.includes('Shopify')){
+       shopify = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Shopify;
+            });
+             console.log(shopify[0].Shopify);
+           setnewShopifyMonthly(shopify[0].Shopify);
+       }
+       if(!newChannelList.includes('Shopify')){
+      setnewShopifyMonthly(0);
+       }
+       let  mer=[];
+       if(newChannelList.includes('MercadoLibre')){
+       mer = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.MercadoLibre;
+            });
+             
+           setnewMercadoOrdersMonthly(mer[0].MercadoLibre);
+       }
+       if(!newChannelList.includes('MercadoLibre')){
+        setnewMercadoOrdersMonthly(0);
+       }  
+       let  pari=[];
+       if(newChannelList.includes('Paris')){
+       pari = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Paris;
+            });
+             
+           setnewParisOrders(pari[0].Paris);
+       }
+       if(!newChannelList.includes('Paris')){
+      setnewParisOrders(0);
+       }   
+       let  exi=[];
+       if(newChannelList.includes('Exito')){
+       exi = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.Exito;
+            });
+             
+          setnewExtitoOrders(exi[0].Exito);
+       }
+       if(!newChannelList.includes('Exito')){
+    setnewExtitoOrders(0);
+       }  
+       let  lista=[];
+       if(newChannelList.includes('ListaTienda')){
+       lista = obj.resume.stackedOrderQtyGraphByMonth.filter((item) => {
+              return item.ListaTienda;
+            });
+             
+          setnewListaOrders(lista[0].ListaTienda);
+       }
+       if(!newChannelList.includes('ListaTienda')){
+   setnewListaOrders(0);
+       }  
+       setisStackedOrdersGraph(false);
+      })
+      .catch((error) => console.log("error", error));
+  };
 //FETCH STACKED GRAPH FOR SALES GRAPH
 const fetchStackedGraphForSales = () => {
   setisStackedGraphLoading(true);
@@ -701,6 +858,7 @@ const fetchStackedGraphForSales = () => {
     setmixedChartData(MIXED);
   };
   const fetchResumenGraphDetails = () => {
+    setisPieChartLoading(true);
     var myHeaders = new Headers();
     myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
     myHeaders.append(
@@ -721,6 +879,7 @@ const fetchStackedGraphForSales = () => {
       .then((result) => {
         var obj = JSON.parse(result);
         setnewData(obj.resume.lineAndBarChart);
+        setisPieChartLoading(false);
       })
       .catch((error) => console.log("error", error));
   };
@@ -1356,7 +1515,7 @@ const fetchStackedGraphForSales = () => {
                   ></MixedAndPieChart>
                   <Col id="ColPieChart" lg="5" md="12" sm="12">
                     <Card id="pieChartCard">
-                      <PieChart linioPie = {linioPie} vtexPie={vtexPie} shopifyPie={shopifyPie} ripleyPie={ripleyPie} magentoPie={magentoPie} wooPie={wooPie}  chambasPie={chambasPie} mercadoPie={mercadoPie} exitoPie={exitoPie} parisPie={parisPie} listaPie={listaPie}></PieChart>
+                      <PieChart isPieChartLoading={isPieChartLoading}linioPie = {linioPie} vtexPie={vtexPie} shopifyPie={shopifyPie} ripleyPie={ripleyPie} magentoPie={magentoPie} wooPie={wooPie}  chambasPie={chambasPie} mercadoPie={mercadoPie} exitoPie={exitoPie} parisPie={parisPie} listaPie={listaPie}></PieChart>
                       <CardFooter>
                         <SalesCard
                           channel={cR}
@@ -1381,13 +1540,19 @@ const fetchStackedGraphForSales = () => {
                   <Col md="6">
                     <Card className="card-chart">
                       <StackedOrderGraph
-                        cR={cR}
-                        channelId={channelId}
                         stackedDateLabel={stackedDateLabel}
-                        storeId={storeId}
-                        selectedDateTo={selectedDateTo}
-                        selectedDateFrom={selectedDateFrom}
-                        countryId={countryId}
+                        newlinioMonthly={newlinioMonthly}
+                        newVtexMonthly={newVtexMonthly}
+                        newRipleyMonthly={newRipleyMonthly}
+                        newChambasMonthly={newChambasMonthly}
+                        newMagentoMonthly={newMagentoMonthly}
+                        newWooCommerceMonthly={newWooCommerceMonthly}
+                        newShopifyMonthly={newShopifyMonthly}
+                        newMercadoOrdersMonthly={newMercadoOrdersMonthly}
+                        newParisOrders={newParisOrders}
+                        newExtitoOrders={newExtitoOrders}
+                        newListaOrders={newListaOrders}
+                        isStackedOrdersGraph={isStackedOrdersGraph}
                       ></StackedOrderGraph>
                       <CardFooter>
                         <StackedGraphOrderCard
@@ -1429,7 +1594,7 @@ const fetchStackedGraphForSales = () => {
                           newParisSales={newParisSales} 
                          newExitoSalesMonthly={newExitoSalesMonthly}
                          newListaSales={ newListaSales}
-                         setisStackedGraphLoading={setisStackedGraphLoading}
+                         isStackedGraphLoading={isStackedGraphLoading}
                         ></StackedSalesGraph>
                         <CardFooter>
                           <StackedGraphSalesCard
