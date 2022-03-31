e401733281e4da2d12e871fe616eb7bc2094e570
shinyKavery <kaverypandanda@instancelatam.com>
2022-03-31 10:42:04 -0300

    corrected the mixed chart graph of pdf

--- src/components/MixedChartReport/ChartMixed.jsx
+++ src/components/MixedChartReport/ChartMixed.jsx
@@ -18,6 +18,93 @@ import "./ChartMixed.css";
 import MixedAndPieChart from "../../components/GraphComponent/mixed-and-pie-chart";
 
 const ChartMixed = (props) => {
+  console.log(props);
+  let MIXED = {
+    labels: props.mixedChartData.labels,
+    datasets: [
+      {
+        label: "Ventas",
+        data: props.mixedChartData.datasets[0].data,
+        backgroundColor: "#344FD5",
+        borderRadius: 5,
+        order: 1,
+      },
+      {
+        label: "Órdenes",
+        yAxisID: "Ordenes",
+
+        data: props.mixedChartData.datasets[1].data,
+        backgroundColor: "#06CBC1",
+        borderColor: "#06CBC1",
+        fill: false,
+        pointHoverRadius: 10,
+        pointHoverBorderWidth: 5,
+        type: "line",
+        order: 0,
+        color: "#9f9f9f",
+      },
+    ],
+
+    options: {
+      responsive: true,
+      // maintainAspectRatio: false,
+      aspectRatio: 2,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        grid: {
+          drawBorder: false,
+          display: false,
+          zeroLineColor: "transparent",
+        },
+        y: {
+          display: true,
+          position: "right",
+          ticks: {
+            color: "#9f9f9f",
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            callback: function (data) {
+              let number = data;
+              let totalValueFormatted = new Intl.NumberFormat("es-CL", {
+                style: "currency",
+                currency: "CLP",
+              }).format(number);
+              return totalValueFormatted;
+            },
+            // padding: 100,
+          },
+          grid: {
+            zeroLineColor: "transparent",
+            display: false,
+            drawBorder: false,
+            color: "#EBEBEBf",
+            // borderDash: [8, 6],
+            lineWidth: 0,
+          },
+        },
+
+        x: {
+          display: true,
+          grid: {
+            display: false,
+            drawBorder: false,
+          },
+          ticks: {
+            font: {
+              size: 10,
+            },
+            padding: 20,
+            color: "#9f9f9f",
+            //  color:"blue",
+          },
+        },
+      },
+    },
+  };
   return (
     <div style={{ margin: "30px" }}>
       <Card
@@ -32,9 +119,14 @@ const ChartMixed = (props) => {
           <p className="card-category"> </p>
         </CardHeader>
         <CardBody>
-          <MixedAndPieChart
+          {/* <MixedAndPieChart
             mixedChartData={props.mixedChartData}
-          ></MixedAndPieChart>
+          ></MixedAndPieChart> */}
+           <Bar
+            data={MIXED}
+            options={MIXED.options}
+            style={{ width: "400px", height: "300px" }}
+          />
         </CardBody>
         <br></br>
         <br></br>
