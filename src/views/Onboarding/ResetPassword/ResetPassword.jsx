ac38d213613f83ddb056ca68ffbee4609034ea19
valvidela <valeriavidela@instancelatam.com>
2022-04-05 15:36:47 -0400

    Add new font
    Add SFUI Display font to the project

--- src/views/Onboarding/ResetPassword/ResetPassword.jsx
+++ src/views/Onboarding/ResetPassword/ResetPassword.jsx
@@ -1,8 +1,11 @@
 import NavBar from 'components/Navbars/InstanceNavbar/Navbar'
 import React from 'react'
 import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap'
+import IconRectangle from '../../../assets/img/icons/Reports/iconRectangle.png'
+import IconBox from '../../../assets/img/icons/Reports/iconBoxs.png'
 import './ResetPassword.css'
-import '../../../'
+
+
 
 const ResetPassword = () => {
   return (
@@ -10,11 +13,43 @@ const ResetPassword = () => {
       <NavBar />
       <div className="backgroundImage">
         <div className="container">
-          {/* <div className="boxForm"> */}
+          <Container style={{display:"flex", justifyContent:"center", marginTop:"15em"}}>
+            <div className="row">     
+              <div className="col-12 col-lg-12"> 
+                <div style={{display:"flex", flexDirection:"column", alignItems:"center"}} >
+                <img src={IconBox} alt="Icon"  style={{marginBottom:"2em"}} width="40" height="auto" />
+                <p className="welcomeText">Bienvenido</p>
+                <p className="titleNameUser">Tennynson Vásquez</p>
+                <img src={IconRectangle} alt="Icon" className="iconRectangle" width="40" height="auto" />  
+                </div>
+                <br/>  
+                
+              </div>
+            </div>    
+          </Container>
+          <Container style={{marginTop:"3em"}}>
+            <Row>
+              <Col>
+                Datos de tu negocio
+              </Col>
+              <Col>
+                Tiendas
+              </Col>
+              <Col>
+                Backoffice
+              </Col>
+              <Col>
+                Fulfillment
+              </Col>
+              <Col>
+                Cliente
+              </Col>
+            </Row>
+          </Container>
           <Container style={{display:"flex", justifyContent:"center"}}>
             <Row>
             <div className="titleBox"> 
-              <h5>
+              <h5 className='titleFont'>
                 Establecer nueva contraseña
               </h5> 
             </div>
@@ -22,27 +57,31 @@ const ResetPassword = () => {
                 <Col md={12}>
                   <FormGroup>
                     <Label className="titleLabel" for="Email">Correo</Label>
-                    <Input style={{borderRadius:"17px"}} type="email" name="email" id="exampleEmail" placeholder="Ingresa aquí tu correo" />
+                    <Input style={{borderRadius:"17px", fontSize:"12px", marginBottom:"2em"}} type="email" name="email" id="exampleEmail" placeholder="Ingresa aquí tu correo" />
                   </FormGroup>
                 </Col>
                 <Col md={12}>
                   <FormGroup>
                     <Label className="titleLabel" for="Password">Contraseña actual</Label>
-                    <Input style={{borderRadius:"17px"}} type="password" name="password" id="examplePassword" placeholder="Ingresa aquí tu contraseña actual" />
+                    <Input style={{borderRadius:"17px", fontSize:"12px", marginBottom:"2em"}} type="password" name="password" id="examplePassword" placeholder="Ingresa aquí tu contraseña actual" />
                   </FormGroup>
                 </Col>
                 <Col md={12}>
                   <FormGroup>
                     <Label className="titleLabel" for="Password">Nueva contraseña</Label>
-                    <Input style={{borderRadius:"17px"}} type="password" name="password" id="examplePassword" placeholder="Ingresa aquí tu nueva contraseña" />
+                    <Input style={{borderRadius:"17px", fontSize:"12px", marginBottom:"2em"}} type="password" name="password" id="examplePassword" placeholder="Ingresa aquí tu nueva contraseña" />
                   </FormGroup>
                 </Col>
-                <Button>Siguiente</Button>
               </Form>
               </Row>
-              </Container>
-            {/* </div> */}
+          </Container>
+           
+              <div className="d-grid gap-12 col-12" style={{justifyItems:"center"}}>
+                <Button className="bttnSiguiente">Siguiente</Button>
+              </div>
+   
         </div>
+     
       </div>
     </div>
   )
