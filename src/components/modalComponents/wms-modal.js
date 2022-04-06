f5104fd07794ee02e68d5a063838e3d73f00d672
unknown <kaverypandanda@instancelatam.com>
2022-04-06 09:22:24 -0400

    added a new modal

--- src/components/modalComponents/wms-modal.js
+++ src/components/modalComponents/wms-modal.js
@@ -4,6 +4,7 @@
 // import errorButton from  '../../assets/img/errorSyncro.png';
 // import { FormGroup, Label, Input } from "reactstrap";
 import VerticalModal from "components/UI/vertical-modal";
+import RadioInput from 'components/UI/radio-input';
 const WmsModal=(props)=>{
     return (
       
@@ -20,22 +21,54 @@ const WmsModal=(props)=>{
             
            <img style={{float:'left'}} src={ellipse}/>  
            <p style={{paddingLeft:'40px', fontWeight: "500",size:'16px',fontStyle:'Medium',lineHeight:'19px', wordSpacing: '10px'}}>Producto ya en bodega,volver a sincronizar</p>
-          
          
+          
            </div>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
-          <h1>hello</h1>
+         
+           <h5 style={{ fontWeight: "700", size: "14px", marginTop:'30px'}}>Cambiar de Bodega</h5>
+            <div  className={classes.configure}>
+         
+           <div style={{top: '20px',
+                bottom: '45%',
+                left: '1%',width:'100%'}}>
+            
+           <img style={{float:'left'}} src={ellipse}/>  
+           <p style={{paddingLeft:'40px', fontWeight: "500",size:'16px',fontStyle:'Medium',lineHeight:'19px', wordSpacing: '10px'}}>Producto ya en bodega,volver a sincronizar</p>
+         
+          
+           </div>
+           <div style={{top: '20px',
+                bottom: '45%',
+                left: '1%',width:'100%'}}>
+            
+           <img style={{float:'left'}} src={ellipse}/>  
+           <p style={{paddingLeft:'40px', fontWeight: "500",size:'16px',fontStyle:'Medium',lineHeight:'19px', wordSpacing: '10px'}}>Producto ya en bodega,volver a sincronizar</p>
+         
+          
+           </div>
+         {/* <RadioInput></RadioInput> */}
+         <div style={{top: '20px',
+                bottom: '45%',
+                left: '1%',width:'100%'}}>
+            
+           <img style={{float:'left'}} src={ellipse}/>  
+           <p style={{paddingLeft:'40px', fontWeight: "500",size:'16px',fontStyle:'Medium',lineHeight:'19px', wordSpacing: '10px'}}>Producto ya en bodega,volver a sincronizar</p>
+         
+          
+           </div>
           </div>
+       </div>
+         {/* <h1>hi</h1>
+         <h1>input 1</h1> */}
+         {/* <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1>
+         <h1>hi</h1> */}
+         {/* <RadioInput></RadioInput> */}
         </VerticalModal>)
 }
 export default WmsModal;
