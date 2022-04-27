3f47133fbb4f2542de10b200230465e6a3249c43
unknown <kaverypandanda@instancelatam.com>
2022-04-27 15:08:51 -0400

    connected the table to the get api

--- src/components/ProfileTableHandler/profile-table-handler.js
+++ src/components/ProfileTableHandler/profile-table-handler.js
@@ -70,40 +70,39 @@ const ProfileTableHandler = () => {
   }, []);
   const fetchProfileDetails = async () => {
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
-    myHeaders.append(
-      "Authorization",
-      "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
-    );
-    myHeaders.append("Content-Type", "application/json");
+myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
+myHeaders.append("Authorization", "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6");
 
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-      headers: myHeaders,
-    };
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+
     try {
       const response = await fetch(
-        "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/users?user=test@test.cl",
+        "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/users?user=test@test.cl",requestOptions
       );
 
       if (!response.ok) {
         throw new Error();
       }
       const data = await response.json();
-      const loadedData = [];
-      for (const key in data) {
-        loadedData.push({
-          id: key,
-          name: data[key].firts_name,
-          mail: data[key].email,
-          profile: data[key].profile,
-          countries: data[key].countries,
-          enabled: data[key].enabled,
-        });
-      }
-      console.log(loadedData);
-      settableProfileData(loadedData);
+      console.log(data);
+      // const loadedData = [];
+      // for (const key in data) {
+      //   loadedData.push({
+      //     id: key,
+      //     name: data[key].firts_name,
+      //     mail: data[key].email,
+      //     profile: data[key].profile,
+      //     countries: data[key].countries,
+      //     enabled: data[key].enabled,
+      //   });
+      // }
+      // console.log(loadedData);
+      settableProfileData(data);
     } catch (error) {
       console.log(error);
     }
@@ -125,7 +124,7 @@ const ProfileTableHandler = () => {
   const columns = [
     {
       title: "Id",
-      field: "id",
+      field: "_id",
       width: "13%",
       headerStyle: {
         backgroundColor: "#1D308E",
@@ -136,7 +135,7 @@ const ProfileTableHandler = () => {
     },
     {
       title: "Nombre",
-      field: "name",
+      field: "first_name",
       width: "13%",
 
       headerStyle: {
@@ -147,7 +146,7 @@ const ProfileTableHandler = () => {
     },
     {
       title: "Correo",
-      field: "mail",
+      field: "email",
       width: "13%",
       headerStyle: {
         backgroundColor: "#1D308E",
@@ -159,6 +158,34 @@ const ProfileTableHandler = () => {
       title: "Tipo de Usuario",
       field: "profile",
       width: "13%",
+      render: (rowData) => {
+        if (rowData != undefined) {
+          if(rowData.profile === 1){
+            return (
+         
+              <div>
+              Administrador
+              </div>
+            );
+          }
+          if(rowData.profile === 2){
+            return (
+         
+              <div>
+             KAM
+              </div>
+            );
+          }
+          if(rowData.profile === 3){
+            return (
+         
+              <div>
+             Cliente
+              </div>
+            );
+          }
+        }
+      },
       headerStyle: {
         backgroundColor: "#1D308E",
         color: "#FFF",
