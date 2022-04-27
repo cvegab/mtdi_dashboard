d1ae8a7b554f89b30bab215d3add1cc91bce359a
unknown <kaverypandanda@instancelatam.com>
2022-04-27 10:58:28 -0400

    added a get api

--- src/components/ProfileTableHandler/profile-table-handler.js
+++ src/components/ProfileTableHandler/profile-table-handler.js
@@ -69,9 +69,22 @@ const ProfileTableHandler = () => {
     fetchProfileDetails();
   }, []);
   const fetchProfileDetails = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
+    myHeaders.append(
+      "Authorization",
+      "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
+    );
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
     try {
       const response = await fetch(
-        "https://profile-table-default-rtdb.firebaseio.com/profile.json"
+        "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/users?user=test@test.cl",
       );
 
       if (!response.ok) {
