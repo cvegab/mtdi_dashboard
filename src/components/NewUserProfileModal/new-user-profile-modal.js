b28e11b33a6bb51fcddcb40dd0a9547f4e3e01a5
unknown <kaverypandanda@instancelatam.com>
2022-04-27 15:36:12 -0400

    added create new users logic

--- src/components/NewUserProfileModal/new-user-profile-modal.js
+++ src/components/NewUserProfileModal/new-user-profile-modal.js
@@ -45,11 +45,18 @@ setName(event.target.value);
    const editProfileHandler = ()=>{
     const profile = {
       // firts_name: nameRef.current.value,
-      firts_name: name,
-      email: emailRef.current.value,
-      profile: userType.current.value,
-      countries:[1,2],
-      stores:[1,2,3]
+      // firts_name: name,
+      // email: emailRef.current.value,
+      // profile: userType.current.value,
+      // countries:[1,2],
+      // stores:[1,2,3]
+      "first_name": name,
+      "last_name": " ",
+      "email": emailRef.current.value,
+      "profile": userType.current.value,
+      "stores": [2,3,6],
+      "countries": [1],
+      "enabled": true
     }
     console.log(profile);
    }
@@ -59,36 +66,41 @@ setName(event.target.value);
     const addProfileHandler = async () => {
         const profile = {
               // firts_name: nameRef.current.value,
-              firts_name: name,
+              // firts_name: name,
+              // email: emailRef.current.value,
+              // profile: userType.current.value,
+              // countries:[1,2],
+              // stores:[1,2,3],
+              // enabled: selfServiceType
+              first_name: name,
+              last_name: " ",
               email: emailRef.current.value,
               profile: userType.current.value,
-              countries:[1,2],
-              stores:[1,2,3],
+              stores: [2,3,6],
+              countries: [1],
               enabled: selfServiceType
             }
-console.log(profile);
+         console.log(profile);
        setprofileDetails(profile);   
     
-        try {
-          const response = await fetch('https://profile-table-default-rtdb.firebaseio.com/profile.json',{
-          method: 'POST',
-          body: JSON.stringify(profile),
-          headers:{
-         'Content-Type': 'application/json'
-          }
-        }
-      );
-         
-          if (!response.ok) {
-            throw new Error();
-          }
-          const data = await response.json();
-          console.log(data);
-          console.log(profileDetails);
-         window.location.reload();
-        } catch (error) {
-          console.log(error);
-        }
+       var myHeaders = new Headers();
+       myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
+       myHeaders.append("Authorization", "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6");
+       myHeaders.append("Content-Type", "text/plain");
+       
+       var raw = profile;
+       
+       var requestOptions = {
+         method: 'POST',
+         headers: myHeaders,
+         body: JSON.stringify(profile),
+         redirect: 'follow'
+       };
+       
+       fetch("https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/user?user=test@test.cl", requestOptions)
+         .then(response => response.text())
+         .then(result => console.log(result))
+         .catch(error => console.log('error', error));
       };
  
 return(
