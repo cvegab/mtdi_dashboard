6de935c5aa30cbb42b9fdeaa6f928ed0ddb47ed2
unknown <kaverypandanda@instancelatam.com>
2022-05-11 09:59:08 -0400

    added the stylings of the data studio iframe

--- src/views/dataStudio/data-studio.js
+++ src/views/dataStudio/data-studio.js
@@ -1,6 +1,25 @@
-const DataStudio = () =>{
-    return(
-        <iframe  src="https://datastudio.google.com/embed/reporting/6caf6c50-7e30-4d78-86aa-f88eb122cb4d/page/RkYTC" style={{position:'relative', top:'0', left:'0', bottom:'0',right:'0', width:'80%', height:'100%', border:'none',marginLeft:'250px', padding:'0',overflow:'hidden', zIndex:'999999'}}></iframe>
-    )
-    }
-    export default DataStudio;
+const DataStudio = () => {
+  return (
+    <div style={{ backgroundColor: "white",width:'100%',height:'100%' }}>
+      <iframe
+        src="https://datastudio.google.com/embed/reporting/6caf6c50-7e30-4d78-86aa-f88eb122cb4d/page/RkYTC"
+        style={{
+          position: "relative",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          width: "95%",
+          height: "100%",
+          marginLeft: "50px",
+          border: "none",
+          padding: "0",
+          overflow: "hidden",
+          zIndex: "999999",
+        }}
+      ></iframe>
+    </div>
+  );
+};
+export default DataStudio;
+// marginLeft:'250px',
