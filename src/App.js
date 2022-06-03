787b615e31cffd3f9b5fd357bf2db5351c6a418c
unknown <kaverypandanda@instancelatam.com>
2022-06-03 19:00:19 -0400

    added the develop api

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   const getUserType = async () => {
     console.log("hi");
     var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "2Rr4OFKHVL98TtlOCUnuNaA2v5w01Z11aI9vdQYJ");
+    myHeaders.append("x-api-key", "3pTvuFxcs79dzls8IFteY5JWySgfvswL9DgqUyP8");
     myHeaders.append(
       "Authorization",
       "Bearer 75b430ce008e4f5b82fa742772e531b71bb11aeb53788098ec769aeb5f58b2298c8d65fa2e4a4a04e3fbf6fb7b0401e6eada7b8782aeca5b259b38fa8b419ac6"
@@ -43,7 +43,7 @@ const App = () => {
 
     try {
       const response = await fetch(
-        "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/preproduction/users?user=sofiavatar@chambas.cl&zeek",
+        "https://32q0xdsl4b.execute-api.sa-east-1.amazonaws.com/develop/users?user=sofiavatar@chambas.cl&zeek",
         requestOptions
       );
 
